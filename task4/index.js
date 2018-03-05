ac63910c85b774fcffab8a4bcdc3dd26478ce6ae
Kate <Katerina639>
2018-03-05 03:36:52 +0300

    addtask4

---
+++ task4/index.js
@@ -0,0 +1,414 @@
+var photoPosts = [{
+        id: '1',
+        description: 'Love this town! Wonderful holidays!',
+        createdAt: new Date(2018, 02, 19, 23, 12),
+        author: 'Kate Kunts',
+        photoLink: 'https://thenypost.files.wordpress.com/2016/07/statue_liberty1a.jpg?quality=90&strip=all&strip=all',
+        Tags: ['#NY', '#StatueOfLiberty'],
+        like: ['Julia', 'Boris', 'Andrey', 'Alexandr']
+    },
+    {
+        id: '2',
+        description: 'Nice view :) Shri-Lanka!!!',
+        createdAt: new Date(2018, 02, 18, 23, 12),
+        author: 'Kate Kunts',
+        photoLink: 'https://www.royalindia.travel/management/mimages/MCAT2-44-Splendid-Goa-Tour-Package.jpg',
+        Tags: [],
+        like: ['Julia', 'Boris', 'Andrey']
+    },
+    {
+        id: '3',
+        description: 'Beach in Shri-Lanka!',
+        createdAt: new Date(2018, 02, 18, 23, 12),
+        author: 'Ketko Evgenia',
+        photoLink: 'http://notgoaway.com/wp-content/uploads/2017/07/Beach-Wallpaper-61.jpg',
+        Tags: [],
+        like: ['Julia', 'Boris', 'Andrey', 'Kate']
+    },
+    {
+        id: '4',
+        description: 'My dear friend, Alisa.',
+        createdAt: new Date(2018, 02, 18, 23, 12),
+        author: 'Boris Serenkov',
+        photoLink: 'http://www.beautiful-views.net/views/camera-photographic-flowers-girl-forest-way.jpg',
+        Tags: [],
+        like: ['Julia', 'Boris', 'Andrey', 'Kate']
+    },
+    {
+        id: '5',
+        description: 'My fauvorite actor',
+        createdAt: new Date(2018, 02, 17, 23, 12),
+        author: 'Ginzburg Andrey',
+        photoLink: 'http://cdn.collider.com/wp-content/uploads/daniel_radcliffe_01.jpg',
+        Tags: ['#HarryPotter'],
+        like: ['Julia', 'Boris', 'Kate']
+    },
+    {
+        id: '6',
+        description: 'Wow! I have met him today in the forest! Nice to met you dear deer :)',
+        createdAt: new Date(2018, 02, 16, 23, 12),
+        author: 'Ketko Evgenia',
+        photoLink: 'http://dreamicus.com/data/deer/deer-03.jpg',
+        Tags: ['#Deer', '#Winter', '#Home'],
+        like: ['Julia', 'Andrey', 'Kate']
+    },
+    {
+        id: '7',
+        description: 'Great morning photo!',
+        createdAt: new Date(2018, 02, 14, 23, 12),
+        author: 'Kate Kunts',
+        photoLink: 'http://лесные.рф/upload/000/u1/2c/3a/rjabina-zimoi-photo-atmosfera.jpg',
+        Tags: ['#Currant', '#Winter'],
+        like: ['Julia', 'Boris', 'Andrey', 'Evgenia', 'Kate']
+    },
+    {
+        id: '8',
+        description: 'Imagine Dragons! Perfect concert! I would never forget it!',
+        createdAt: new Date(2018, 02, 14, 23, 12),
+        author: 'Kakoi-to Chel',
+        photoLink: 'http://nosalive.com/wp-content/uploads/2016/12/NOS-Alive-Imagine-Dragons-FB-OG.jpg',
+        Tags: [],
+        like: ['Julia', 'Boris', 'Andrey', 'Kate', 'LoL KeK Cheburek']
+    },
+    {
+        id: '9',
+        description: 'Really pleased! Love all of you!!!',
+        createdAt: new Date(2018, 02, 13, 23, 12),
+        author: 'Kuzmich Alexandr',
+        photoLink: 'http://www.theholidayspot.com/friendship/images/friendship-day-party-ideas.jpg',
+        Tags: ['#Party', '#Valentine Day'],
+        like: ['Julia', 'Boris', 'Andrey', 'Kate']
+
+    },
+    {
+        id: '10',
+        description: 'My guitar... You are always with me) fantastik music of my life...',
+        createdAt: new Date(2018, 02, 12, 23, 12),
+        author: 'Evgeniy Kukel',
+        photoLink: 'https://truimg.toysrus.com/product/images/disney-pixar-coco-guitar-white--BC077F8A.zoom.jpg',
+        Tags: [],
+        like: ['Julia', 'Boris', 'Andrey', 'Kate']
+
+    },
+    {
+        id: '11',
+        description: 'Love this town! Wonderful holidays!',
+        createdAt: new Date(2017, 02, 19, 23, 12),
+        author: 'Kate Kunts',
+        photoLink: 'https://thenypost.files.wordpress.com/2016/07/statue_liberty1a.jpg?quality=90&strip=all&strip=all',
+        Tags: ['#NY', '#StatueOfLiberty'],
+        like: ['Julia', 'Boris', 'Andrey', 'Alexandr']
+    },
+    {
+        id: '12',
+        description: 'Nice view :) Shri-Lanka!!!',
+        createdAt: new Date(2017, 02, 18, 23, 12),
+        author: 'Kate Kunts',
+        photoLink: 'https://www.royalindia.travel/management/mimages/MCAT2-44-Splendid-Goa-Tour-Package.jpg',
+        Tags: [],
+        like: ['Julia', 'Boris', 'Andrey']
+    },
+    {
+        id: '13',
+        description: 'Beach in Shri-Lanka!',
+        createdAt: new Date(2017, 02, 18, 23, 12),
+        author: 'Ketko Evgenia',
+        photoLink: 'http://notgoaway.com/wp-content/uploads/2017/07/Beach-Wallpaper-61.jpg',
+        Tags: [],
+        like: ['Julia', 'Boris', 'Andrey', 'Kate']
+    },
+    {
+        id: '14',
+        description: 'My dear friend, Alisa.',
+        createdAt: new Date(2017, 02, 18, 23, 12),
+        author: 'Boris Serenkov',
+        photoLink: 'http://www.beautiful-views.net/views/camera-photographic-flowers-girl-forest-way.jpg',
+        Tags: [],
+        like: ['Julia', 'Boris', 'Andrey', 'Kate']
+    },
+    {
+        id: '15',
+        description: 'My fauvorite actor',
+        createdAt: new Date(2017, 02, 17, 23, 12),
+        author: 'Ginzburg Andrey',
+        photoLink: 'http://cdn.collider.com/wp-content/uploads/daniel_radcliffe_01.jpg',
+        Tags: ['#HarryPotter'],
+        like: ['Julia', 'Boris', 'Kate']
+    },
+    {
+        id: '16',
+        description: 'Wow! I have met him today in the forest! Nice to met you dear deer :)',
+        createdAt: new Date(2017, 02, 16, 23, 12),
+        author: 'Ketko Evgenia',
+        photoLink: 'http://dreamicus.com/data/deer/deer-03.jpg',
+        Tags: ['#Deer', '#Winter', '#Home'],
+        like: ['Julia', 'Andrey', 'Kate']
+    },
+    {
+        id: '17',
+        description: 'Great morning photo!',
+        createdAt: new Date(2017, 02, 14, 23, 12),
+        author: 'Kate Kunts',
+        photoLink: 'http://лесные.рф/upload/000/u1/2c/3a/rjabina-zimoi-photo-atmosfera.jpg',
+        Tags: ['#Currant', '#Winter'],
+        like: ['Julia', 'Boris', 'Andrey', 'Evgenia', 'Kate']
+    },
+    {
+        id: '18',
+        description: 'Imagine Dragons! Perfect concert! I would never forget it!',
+        createdAt: new Date(2017, 02, 14, 23, 12),
+        author: 'Kakoi-to Chel',
+        photoLink: 'http://nosalive.com/wp-content/uploads/2016/12/NOS-Alive-Imagine-Dragons-FB-OG.jpg',
+        Tags: [],
+        like: ['Julia', 'Boris', 'Andrey', 'Kate', 'LoL KeK Cheburek']
+    },
+    {
+        id: '19',
+        description: 'Really pleased! Love all of you!!!',
+        createdAt: new Date(2017, 02, 13, 23, 12),
+        author: 'Kuzmich Alexandr',
+        photoLink: 'http://www.theholidayspot.com/friendship/images/friendship-day-party-ideas.jpg',
+        Tags: ['#Party', '#Valentine Day'],
+        like: ['Julia', 'Boris', 'Andrey', 'Kate']
+
+    },
+    {
+        id: '20',
+        description: 'My guitar... You are always with me) fantastik music of my life...',
+        createdAt: new Date(2017, 02, 12, 23, 12),
+        author: 'Evgeniy Kukel',
+        photoLink: 'https://truimg.toysrus.com/product/images/disney-pixar-coco-guitar-white--BC077F8A.zoom.jpg',
+        Tags: [],
+        like: ['Julia', 'Boris', 'Andrey', 'Kate']
+
+    }
+]
+
+
+
+
+var functions = (function() {
+    return {
+        getPhotoPosts(skip = 0, top = 10, filter) {
+
+            if (typeof skip !== 'number') {
+                return;
+            }
+            if (typeof top !== 'number') {
+                return;
+            }
+
+            let photo_post = this;
+            if (typeof filter === 'object') {
+                photo_post = photo_post.filter((photo) => {
+                    if (filter.hasOwnProperty('author')) {
+                        if (filter.author !== photo.author)
+                            return false;
+                    }
+                    if (filter.hasOwnProperty('Date')) {
+                        if (Date.parse(filter.Date) !== Date.parse(photo.createdAt))
+                            return false;
+                    }
+                    if (filter.hasOwnProperty('Tags')) {
+                        if (!filter.Tags.every((tag) => {
+                                return photo.Tags.includes(tag);
+                            }))
+                            return false;
+                    }
+                    return true;
+                });
+            }
+            return photo_post.slice(skip, top + skip);
+        },
+
+        getPhotoPost(id) {
+            if (typeof id !== 'string' || id === '')
+                return;
+            for (var i = 0; i < photoPosts.length; ++i) {
+                if (photoPosts[i].id === id)
+                    return photoPosts[i];
+            }
+            return 'There is no such id '
+        },
+
+
+        validatePhotoPost(photo) {
+            if (typeof photo !== 'object')
+                return false;
+            if (typeof photo.id !== 'string' || photo.id === '')
+                return false;
+            if (typeof photo.description !== 'string' || photo.description === '' || photo.description.length > 200)
+                return false;
+            if (!(photo.createdAt instanceof Date))
+                return false;
+            if (typeof photo.author !== 'string' || photo.author === '')
+                return false;
+            if (typeof photo.photoLink !== 'string' || photo.photoLink === '')
+                return false;
+            if (!Array.isArray(photo.Tags) || !Array.isArray(photo.like))
+                return false;
+            return true;
+        },
+
+        addPhotoPost(photo) {
+            photo.id = this.length.toString();
+            if (!this.validatePhotoPost(photo))
+                return false;
+            this.push(photo);
+            this.sort((photo1, photo2) => {
+                return photo2.createdAt - photo1.createdAt;
+            });
+            return true;
+        },
+
+        editPhotoPost(id, editPost) {
+            if (typeof id !== 'string' || id === '' || typeof editPost !== 'object') {
+                return false;
+            }
+            let post = this.getPhotoPost(id);
+            if (post) {
+                if (editPost.hasOwnProperty('description'))
+                    post.description = editPost.description;
+                if (editPost.hasOwnProperty('photoLink'))
+                    post.photoLink = editPost.photoLink;
+                if (editPost.hasOwnProperty('Tags'))
+                    post.Tags = editPost.Tags;
+                if (editPost.hasOwnProperty('like'))
+                    post.like = editPost.like;
+                return true;
+            }
+            return false;
+        },
+
+
+        removePhotoPost(id) {
+            if (typeof id !== 'string' || id === '') {
+                return false;
+            }
+            let photos = this;
+            for (var i = 0; i < photos.length; ++i) {
+                if (photos[i].id === id) {
+                    photos.splice(i, 1);
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+})();
+
+
+for (method in functions) {
+    photoPosts.__proto__[method] = functions[method];
+}
+
+
+
+
+var filter1 = {
+    author: 'Kate Kunts'
+}
+
+var filter2 = {
+    Tags: ['#Deer']
+}
+
+var filter3 = {
+    Date: new Date(2018, 02, 14, 23, 12)
+}
+
+var filter4 = {
+    author: 'Pups',
+    Date: new Date(2018, 02, 14, 23, 12)
+}
+
+var filter5 = {
+    author: 'Ketko Evgenia',
+    Date: new Date(2017, 02, 16, 23, 12)
+}
+
+
+
+
+console.log("\ntesting getPhotoPosts\n")
+console.log("skip and top = default    :", photoPosts.getPhotoPosts());
+console.log("skip = 4 and top = default:", photoPosts.getPhotoPosts(4));
+console.log("skip = 7 and top = 4      :", photoPosts.getPhotoPosts(7, 4));
+console.log("filtering1     :", photoPosts.getPhotoPosts(0, 10, filter1));
+console.log("filtering2     :", photoPosts.getPhotoPosts(0, 10, filter2));
+console.log("filtering3     :", photoPosts.getPhotoPosts(0, 10, filter3));
+console.log("filtering4     :", photoPosts.getPhotoPosts(0, 10, filter4));
+console.log("filtering5     :", photoPosts.getPhotoPosts(0, 10, filter5));
+console.log("invalid value  :", photoPosts.getPhotoPosts(""))
+
+
+console.log("\ntesting getPhotoPost\n");
+console.log("1 id       :", photoPosts.getPhotoPost('1'));
+console.log("7 id      :", photoPosts.getPhotoPost('7'));
+console.log("invalid id (42):", photoPosts.getPhotoPost('42'));
+
+console.log("\ntesting validatePhotoPost\n");
+console.log("Good post    :", photoPosts.validatePhotoPost({
+    id: '15',
+    description: 'My fauvorite actor',
+    createdAt: new Date(2017, 02, 17, 23, 12),
+    author: 'Ginzburg Andrey',
+    photoLink: 'http://cdn.collider.com/wp-content/uploads/daniel_radcliffe_01.jpg',
+    Tags: ['#HarryPotter'],
+    like: ['Julia', 'Boris', 'Kate']
+}));
+console.log("invalid link:", photoPosts.validatePhotoPost({
+    id: '15',
+    description: 'My fauvorite actor',
+    createdAt: new Date(2017, 02, 17, 23, 12),
+    author: 'Ginzburg Andrey',
+    photoLink: '',
+    Tags: ['#HarryPotter'],
+    like: ['Julia', 'Boris', 'Kate']
+}));
+console.log("invalid date:", photoPosts.validatePhotoPost({
+    id: '15',
+    description: 'My fauvorite actor',
+    createdAt: "nfdv",
+    author: 'Ginzburg Andrey',
+    photoLink: 'http://cdn.collider.com/wp-content/uploads/daniel_radcliffe_01.jpg',
+    Tags: ['#HarryPotter'],
+    like: ['Julia', 'Boris', 'Kate']
+}));
+
+console.log("\ntesting addPhotoPost\n");
+console.log("all posts:", photoPosts);
+console.log("invalid post:", photoPosts.addPhotoPost({
+    description: 'My fauvorite actor',
+    createdAt: new Date(2017, 02, 17, 23, 12),
+    author: '',
+    photoLink: 'http://cdn.collider.com/wp-content/uploads/daniel_radcliffe_01.jpg',
+    Tags: ['#HarryPotter'],
+    like: ['Julia', 'Boris', 'Kate']
+}));
+console.log("all posts:", photoPosts);
+console.log("valid post  :", photoPosts.addPhotoPost({
+    description: 'My fauvorite actor',
+    createdAt: new Date(2017, 02, 17, 23, 12),
+    author: 'Ginzburg Andrey',
+    photoLink: 'http://cdn.collider.com/wp-content/uploads/daniel_radcliffe_01.jpg',
+    Tags: ['#HarryPotter'],
+    like: ['Julia', 'Boris', 'Kate']
+}));
+console.log("all posts:", photoPosts);
+
+
+console.log("\ntesting editPhotoPost\n");
+console.log("4th id now:", photoPosts.getPhotoPost('4'));
+console.log("edit post 4   :", photoPosts.editPhotoPost('4', {
+    photoLink: 'linklinklink',
+    description: 'Nice Day'
+}));
+console.log("post 4 :", photoPosts.getPhotoPost('4'));
+console.log("invalid value        :", photoPosts.editPhotoPost(''));
+
+
+console.log("\ntesting removePhotoPost-");
+console.log("invalid value:", photoPosts.removePhotoPost(''));
+console.log("remove 5th post     :", photoPosts.removePhotoPost('5'));
+console.log("get 5th post :", photoPosts.getPhotoPost('5'));
